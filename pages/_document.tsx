b7175d47292c118e942107fe8e501ffa42ddabd4
Nathan Pannell <n8tewolf@gmail.com>
2024-02-15 20:30:58 -0800

    added navbar

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,24 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <title>Nathan Pannell | AI</title>
+      <meta
+        name="description"
+        content="Nathan Pannell, a dedicated student at the University of Victoria with a passion for web development, artificial intelligence, and data science. Exploring the intersection of technology and innovation."
+      />
+      <meta
+        name="keywords"
+        content="Nathan Pannell, Web Development, AI, Artificial Intelligence, Data Science, University of Victoria"
+      />
+      <meta name="author" content="Nathan Pannell" />
+      <meta property="og:title" content="Nathan Pannell | Web Developer & AI/Data Science Enthusiast" />
+      <meta
+        property="og:description"
+        content="Discover the world of web development, AI, and data science with Nathan Pannell, a student at the University of Victoria."
+      />
+      <meta property="og:type" content="website" />
+      {/* <meta property="og:image" content="" /> */}
+      <body className="font-primary">
         <Main />
         <NextScript />
       </body>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import MobileNav from "@/Components/MobileNav";
+import Nav from "@/Components/Nav";
+import React, { useState } from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [nav, setNav] = useState(false);
+  const openNav = () => setNav(true);
+  const closeNav = () => setNav(false);
+
+  return (
+    <div className="overflow-x-hidden">
+      <div>
+        {/* Navbar */}
+        <MobileNav nav={nav} closeNav={closeNav} />
+        <Nav openNav={openNav} />
+        {/* Hero Section */}
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
